bf844f513263003fb0e4ab9637e117ac91654ec8
JoseAngelCabanes <joseangelcabanes29@gmail.com>
2024-05-14 08:31:06 +0200

    added Vue app2 and mounted in the same html

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const app = Vue.createApp({
   data() {
     return {
-      currentUserInput: '',
-      message: 'Vue is great!',
+      currentUserInput: "",
+      message: "Vue is great!",
     };
   },
   methods: {
@@ -17,22 +17,35 @@ const app = Vue.createApp({
 
 app.mount('#app');
 
+const app2 = Vue.createApp({
+  template: `
+  <p>{{ favoriteMeal }}</p>
+  `,
+  data() {
+    return {
+      favoriteMeal: "Pizza",
+    };
+  },
+});
+
+app2.mount('#app2')
+
 const data = {
-  message: 'Hello!',
-  longMessage: 'Hello! World!'
+  message: "Hello!",
+  longMessage: "Hello! World!",
 };
 
 const handler = {
   set(target, key, value) {
-    if (key === 'message') {
-      target.longMessage = value + 'World!'
+    if (key === "message") {
+      target.longMessage = value + "World!";
     }
     target.message = value;
-  }
+  },
 };
 
 const proxy = new Proxy(data, handler);
 
-proxy.message = 'Hello!!!!'
+proxy.message = "Hello!!!!";
 
 console.log(proxy.longMessage);
